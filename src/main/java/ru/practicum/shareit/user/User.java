2704dd812c406f34aaf085797dae60faf5d8b5bc
Inessa Babenko <iness.tt.eln@gmail.com>
2026-03-26 17:40:56 +0700

    feat: add JPA repository

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,16 +1,25 @@
 package ru.practicum.shareit.user;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@Entity
+@ToString
 @RequiredArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String name;
 
     @NotBlank
--- src/main/java/ru/practicum/shareit/user/dal/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/dal/UserRepository.java
@@ -1,45 +1,12 @@
 package ru.practicum.shareit.user.dal;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.User;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
-    private final HashMap<Long, User> users = new HashMap<>();
-    private Long id = 1L;
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    public Optional<User> getUserById(Long userId) {
-        User user = users.get(userId);
-        return Optional.ofNullable(user);
-    }
-
-    public User saveUser(User user) {
-        user.setId(id);
-        users.put(id, user);
-        id++;
-        return user;
-    }
-
-    public Optional<User> updateUser(Long userId,User user) {
-        if (!users.containsKey(userId)) {
-            return Optional.empty();
-        }
-        user.setId(userId);
-        users.put(userId, user);
-        return Optional.of(user);
-    }
-
-    public void deleteUser(Long userId) {
-        users.remove(userId);
-    }
-
-    public List<String> getEmails() {
-        return users.values().stream().map(User::getEmail).toList();
-    }
+    boolean existsByEmail(String email);
 }
